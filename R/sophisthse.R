e210adad4e0f5d5743b7cca10ed3f69ed3b11d24
Boris Demeshev <boris.demeshev@gmail.crom>
2014-10-18 13:07:30 +0400

    metadata in a table

--- R/sophisthse.R
+++ R/sophisthse.R
@@ -6,6 +6,22 @@
 #' @import XML dplyr RCurl zoo
 NULL
 
+#' Remove slash junk
+#'
+#' Remove slash junk
+#'
+#' Remove slash junk
+#'
+#' @param x the character vector
+#' @return clean character vector
+#' @examples
+#' remove_slash_junk("xxx \n yyy")
+remove_slash_junk <- function(x) {
+  x <- gsub("\n"," ",x)
+  x <- gsub("\r"," ",x)
+  x <- gsub("  "," ",x)
+  return(x)
+}
 
 #' Convert string with a number in Russian tradition in numeric
 #' 
@@ -72,23 +88,49 @@ get_stat_hse_info_vector <- function(series.name = "IP_EA_Q",
     if (length(temp.value)>0) text[i] <- temp.value # avoid empty blocks 
   }
     
-  return(text)
+  return(remove_slash_junk(text))
 }  
-  
+
+
+
+#' Guess series frequency from it's name
+#'
+#' Guess series frequency from it's name for sophist.hse.ru
+#'
+#' Guess series frequency from it's name
+#' by checking presence of letters Y, M, Q
+#'
+#' @param series.name the name of a time series
+#' @return guessed frequency (1/4/12)
+#' @examples
+#' requested_freq("WAG_Y")
+requested_freq <- function(series.name) {
+  req_type <- rep(1,length(series.name)) # we assume yearly ts by default
+  req_type[grepl("_Y$|_Y_",series.name)] <- 1
+  req_type[grepl("_M$|_M_",series.name)] <- 12
+  req_type[grepl("_Q$|_Q_",series.name)] <- 4  
+  return(req_type)
+}
+
+
+
+
+
 #' Obtain time series from sophist.hse.ru
 #'
-#' This function obtains multiple time series from sophist.hse.ru
+#' This function obtains univariate time series from sophist.hse.ru
 #'
-#' If many time series are requested at once they should be of the same frequency.
+#' The output may be choosen to be "zoo" or "data.frame". Metadata is saved
+#' into the attribute "metadata". 
 #'
 #' @param series.name the names of the time series, i.e. "WAG_Y"
 #' @param output the desired output format, either 'zoo' or 'data.frame'
 #' @return data.frame with the corresponding time series
 #' @export
 #' @examples
-#' df <- sophisthse("IP_EA_Q")
-#' df <- sophisthse("WAG_Y")
-sophisthse <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame")) {  
+#' df <- sophisthse0("IP_EA_Q")
+#' df <- sophisthse0("WAG_Y")
+sophisthse0 <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame")) {  
   
   # download main data
   url <- paste("http://sophist.hse.ru/exes/tables/",series.name,".htm",sep="")
@@ -107,12 +149,13 @@ sophisthse <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame"))
   for (i in 1:ncol(df)) df[,i] <- as.character(df[,i])
   
   # save units of measure 
-  attr(df,"units") <- gsub("&nbsp","",df[1,])
+  metadata <- data_frame(tsname = colnames(df))
+  metadata$unit <- gsub("&nbsp","",df[1,])
   
   # get full variable names
   full.names <- rep("",ncol(df))
   for (i in 2:ncol(df)) full.names[i] <- xmlValue(url.root[[3]][[1]][[1]][[i-1]])
-  attr(df,"full.names") <- full.names
+  metadata$fullname <- remove_slash_junk(full.names)
   
   # remove unused lines (units, info about series)
   df <- df[2:(nrow(df)-4),]
@@ -128,6 +171,11 @@ sophisthse <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame"))
   t.type <- 1 # by default we assume early data
   if (length(grep("[IV]",df$T))>1) t.type <- 4 # quarterly data
   if (length(grep("^[23456789]$",df$T))>1) t.type <- 12 # monthly data
+  
+  req.type <- requested_freq(series.name)
+  if (! req.type == t.type) 
+    warning("The requested frequency (",req.type,
+            ") does not match detected frequency (", t.type,")")
   
   # convert data to correct format
   if (t.type==1) df$T <- as.numeric(df$T)
@@ -147,21 +195,26 @@ sophisthse <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame"))
     df$T <- start.date + seq(from=0,by=1/4,length=nrow(df))
   }
     
-  # get methodology, comment and source
 
+  
+  
+  
+  # get methodology, comment and source
+    
   n.vars <- ncol(df)-1 # remove "T", the name of index
-  						
+  						  
+  metadata$methodology <- 
+     c("",get_stat_hse_info_vector(series.name,n.vars,"methodology"))
+  metadata$source <- 
+    c("",get_stat_hse_info_vector(series.name,n.vars,"source"))
+  metadata$comment <- 
+    c("",get_stat_hse_info_vector(series.name,n.vars,"comment"))
+  
+  
   if (output[1] == "zoo") 
     df <- zoo( select(df, -T), order.by = df$T,
-                 frequency = t.type)
-  
-  attr(df,"methodology") <- c("",
-                              get_stat_hse_info_vector(series.name,n.vars,"methodology"))
-  attr(df,"source") <- c("",
-                         get_stat_hse_info_vector(series.name,n.vars,"source"))  
-  attr(df,"comment") <- c("",
-                          get_stat_hse_info_vector(series.name,n.vars,"comment"))
-    
+               frequency = t.type)
+  attr(df,"metadata") <- metadata  
   
   return(df)  
 }

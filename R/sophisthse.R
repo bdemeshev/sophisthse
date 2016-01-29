9b6995f9d75087b44e9c85ac86ccbca7a35a1ba3
Boris Demeshev <boris.demeshev@gmail.com>
2016-01-29 11:14:35 +0300

    test encoding

--- R/sophisthse.R
+++ R/sophisthse.R
@@ -2,7 +2,7 @@
 #'
 #' @name sophisthse
 #' @docType package
-#' @author Boris Demeshev 
+#' @author Boris Demeshev
 #' @import XML dplyr RCurl zoo stringr
 NULL
 
@@ -24,12 +24,12 @@ remove_slash_junk <- function(x) {
 }
 
 #' Convert string with a number in Russian tradition in numeric
-#' 
+#'
 #' This function is useful for automatic conversion of strings to numeric
 #'
-#' Russian standards prescribes to use comma as a decimal separator. 
+#' Russian standards prescribes to use comma as a decimal separator.
 #' This function removes spaces and converts string to number.
-#' 
+#'
 #' @param x the string with the number
 #' @return numeric the number converted from the string
 #' @export
@@ -62,34 +62,39 @@ get_stat_hse_info_vector <- function(series.name = "IP_EA_Q",
                                      n.vars = 1,
                                      info = c("methodology","source","comment")) {
 
-  
-  if (info[1]=="methodology") 
+  info <- match.arg(info)
+
+  if (info=="methodology")
     url <- paste("http://sophist.hse.ru/hse/1/met/",series.name,".html",sep="")
-  if (info[1]=="source") 
+  if (info=="source")
     url <- paste("http://sophist.hse.ru/hse/1/sor/",series.name,".html",sep="")
-  if (info[1]=="comment") 
+  if (info=="comment")
     url <- paste("http://sophist.hse.ru/hse/1/com/",series.name,".html",sep="")
-  
+
   url.html <- getURL(url,.encoding="UTF-8")
   url.parsed <- htmlTreeParse(url.html)
   url.root <- xmlRoot(url.parsed)
 
-  
+
   # there maybe 2 situations:
   # one entry for each variable
   # one entry for all variables
   # or more than 2 ;)
-  
+
   n.on.site <- length(xmlChildren(url.root[[3]][[3]])) %/% 2 # only approximate
   text <- rep("",n.vars)
-  
+
   for (i in 1:min(n.on.site,n.vars)) {
     temp.value <- xmlValue(url.root[[3]][[3]][[2*i]])
-    if (length(temp.value)>0) text[i] <- temp.value # avoid empty blocks 
+    if (length(temp.value)>0) text[i] <- temp.value # avoid empty blocks
   }
-    
-  return(remove_slash_junk(text))
-}  
+
+  text <- remove_slash_junk(text)
+  text <- as.character(text)
+  Encoding(text) <- "UTF-8"
+
+  return(text)
+}
 
 
 
@@ -108,7 +113,7 @@ requested_freq <- function(series.name) {
   req_type <- rep(1,length(series.name)) # we assume yearly ts by default
   req_type[grepl("_Y$|_Y_",series.name)] <- 1
   req_type[grepl("_M$|_M_",series.name)] <- 12
-  req_type[grepl("_Q$|_Q_",series.name)] <- 4  
+  req_type[grepl("_Q$|_Q_",series.name)] <- 4
   return(req_type)
 }
 
@@ -121,7 +126,7 @@ requested_freq <- function(series.name) {
 #' This function obtains univariate time series from sophist.hse.ru
 #'
 #' The output may be choosen to be "zoo" or "data.frame". Metadata is saved
-#' into the attribute "metadata". 
+#' into the attribute "metadata".
 #'
 #' @param series.name the names of the time series, i.e. "WAG_Y"
 #' @param output the desired output format, either 'zoo' or 'data.frame'
@@ -130,53 +135,53 @@ requested_freq <- function(series.name) {
 #' @examples
 #' df <- sophisthse0("IP_EA_Q")
 #' df <- sophisthse0("WAG_Y")
-sophisthse0 <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame")) {  
-  
+sophisthse0 <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame")) {
+
   # download main data
   url <- paste("http://sophist.hse.ru/exes/tables/",series.name,".htm",sep="")
   url.html <- getURL(url,.encoding="UTF-8")
-  
+
   # get main table
   tables <- readHTMLTable(url.html)
-  df <- tables[[1]]  
-  
+  df <- tables[[1]]
+
   # html parse
   url.parsed <- htmlTreeParse(url.html)
   url.root <- xmlRoot(url.parsed)
-  
-  
+
+
   # all to character
   for (i in 1:ncol(df)) df[,i] <- as.character(df[,i])
-  
-  # save units of measure 
-  metadata <- data_frame(tsname = colnames(df))
+
+  # save units of measure
+  metadata <- dplyr::data_frame(tsname = colnames(df))
   metadata$unit <- gsub("&nbsp","",df[1,])
-  
+
   # get full variable names
   full.names <- rep("",ncol(df))
   for (i in 2:ncol(df)) full.names[i] <- xmlValue(url.root[[3]][[1]][[1]][[i-1]])
   metadata$fullname <- remove_slash_junk(full.names)
-  
+
   # remove unused lines (units, info about series)
   df <- df[2:(nrow(df)-4),]
-  
+
   # remove spaces, replace "," by ".", convert to numeric
-  for (i in 2:ncol(df)) 
+  for (i in 2:ncol(df))
     df[,i] <- rus2num(df[,i])
-  
-  
+
+
   # pretty time index
-  
+
   # determine the type of data: yearly/quarterly/mothly
   t.type <- 1 # by default we assume early data
   if (length(grep("[IV]",df$T))>1) t.type <- 4 # quarterly data
   if (length(grep("^[23456789]$",df$T))>1) t.type <- 12 # monthly data
-  
+
   req.type <- requested_freq(series.name)
-  if (! req.type == t.type) 
+  if (! req.type == t.type)
     warning("The guessed requested frequency (",req.type,
             ") does not match detected frequency (", t.type,")")
-  
+
   # convert data to correct format
   if (t.type==1) df$T <- as.numeric(df$T)
   if (t.type==12) {
@@ -190,36 +195,36 @@ sophisthse0 <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame")
     df$T <- gsub(" III$","-3",df$T)
     df$T <- gsub(" II$","-2",df$T)
     df$T <- gsub(" I$","-1",df$T)
-    
+
     start.date <- as.yearqtr(df$T[1])
     df$T <- start.date + seq(from=0,by=1/4,length=nrow(df))
   }
-    
 
-  
-  
-  
+
+
+
+
   # get methodology, comment and source
-    
+
   n.vars <- ncol(df)-1 # remove "T", the name of index
-  						  
-  metadata$methodology <- 
+
+  metadata$methodology <-
      c("",get_stat_hse_info_vector(series.name,n.vars,"methodology"))
-  metadata$source <- 
+  metadata$source <-
     c("",get_stat_hse_info_vector(series.name,n.vars,"source"))
-  metadata$comment <- 
+  metadata$comment <-
     c("",get_stat_hse_info_vector(series.name,n.vars,"comment"))
-  
+
   metadata$freq <- t.type
-  
-  if (output[1] == "zoo") 
+
+  if (output[1] == "zoo")
     df <- zoo( select(df, -T), order.by = df$T,
                frequency = t.type)
-  
+
   metadata <- filter(metadata, !tsname=="T")
-  attr(df,"metadata") <- metadata  
-  
-  return(df)  
+  attr(df,"metadata") <- metadata
+
+  return(df)
 }
 
 #' Obtain multivariate time series from sophist.hse.ru
@@ -227,7 +232,7 @@ sophisthse0 <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame")
 #' This function obtains multivariate time series from sophist.hse.ru
 #'
 #' The output may be choosen to be "zoo" or "data.frame". Metadata is saved
-#' into the attribute "metadata". 
+#' into the attribute "metadata".
 #'
 #' @param series.name the names of the time series, i.e. "WAG_Y"
 #' @param output the desired output format, either 'zoo' or 'data.frame'
@@ -236,14 +241,14 @@ sophisthse0 <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame")
 #' @examples
 #' df <- sophisthse("IP_EA_Q")
 #' df <- sophisthse("WAG_Y")
-sophisthse <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame")) {  
+sophisthse <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame")) {
   req_type <- requested_freq(series.name)
   if (length(unique(req_type))>1) warning("Probably requested series have different frequency.")
-  
+
   all_data <- sophisthse0(series.name[1],output ="data.frame")
-  all_meta <- attr(all_data,"metadata")  
+  all_meta <- attr(all_data,"metadata")
   series.name <- series.name[-1]
-  
+
   for (sname in series.name) {
     one_data <- sophisthse0(sname,output ="data.frame")
     one_meta <- attr(one_data,"metadata")
@@ -251,10 +256,10 @@ sophisthse <- function(series.name = "IP_EA_Q", output = c("zoo", "data.frame"))
     all_data <- merge(all_data,one_data,by = "T",all = TRUE)
   }
 
-  if (output[1] == "zoo") 
+  if (output[1] == "zoo")
     all_data <- zoo( select(all_data, -T), order.by = all_data$T,
-               frequency = unique(all_meta$freq)) 
-  
+               frequency = unique(all_meta$freq))
+
   attr(all_data,"metadata") <- all_meta
   return(all_data)
 }
